8787011b15545293fa211c8cd74bfa7a6e35ac91
Danny Yassine <dannyyassine@gmail.com>
2024-03-21 21:36:59 -0400

    clean up tests

--- tests/queue_integration_test.rs
+++ tests/queue_integration_test.rs
@@ -1,19 +1,19 @@
 #[cfg(test)]
 mod tests {
     use dotenvy::dotenv;
-    use rust_queue::models::{
-        job::{Job, JobStatus},
-        queue::Queue,
+    use rust_queue::{
+        models::{
+            job::{Job, JobStatus},
+            queue::Queue,
+        },
+        repositories::job_repository::JobRepository,
     };
-    use std::env;
 
     #[tokio::test]
     async fn it_should_handle_job_in_database() {
         dotenv().ok();
-        let connection = sqlx::PgPool::connect(&env::var("DATABASE_URL").unwrap())
-            .await
-            .unwrap();
-        let _ = sqlx::query("DELETE from jobs;").execute(&connection).await;
+        let job_repository = JobRepository::new().await;
+        job_repository.delete_all_jobs().await;
 
         let job: Job = Job {
             id: 1,
@@ -23,35 +23,17 @@ mod tests {
             data: "".to_string(),
         };
 
-        let _ = sqlx::query(
-            format!(
-                "INSERT INTO jobs (payload, status, model_type, data) VALUES ('{}', '{}', '{}', '{}');",
-                job.payload, job.status, job.model_type, job.data
-            )
-            .as_str(),
-        )
-        .execute(&connection)
-        .await;
+        job_repository.add_job(&job).await;
 
-        let results: Result<Vec<Job>, _> = sqlx::query_as::<_, Job>(
-            "SELECT id, payload, status, model_type, data FROM jobs where status = 'pending'",
-        )
-        .fetch_all(&connection)
-        .await;
-
+        let results: Option<Vec<Job>> = job_repository.get_all_jobs(Some(JobStatus::Pending)).await;
         assert_eq!(results.unwrap().len(), 1);
 
         let mut queue: Queue = Queue::new_with_job_limit(1);
         queue.listen().await;
 
-        let results: Result<Vec<Job>, _> = sqlx::query_as::<_, Job>(
-            "SELECT id, payload, status, model_type, data FROM jobs where status = 'pending'",
-        )
-        .fetch_all(&connection)
-        .await;
-
+        let results: Option<Vec<Job>> = job_repository.get_all_jobs(Some(JobStatus::Pending)).await;
         assert_eq!(results.unwrap().len(), 0);
 
-        let _ = sqlx::query("DELETE from jobs;").execute(&connection).await;
+        job_repository.delete_all_jobs().await;
     }
 }
