e990b4b927e99f2c660c5ef0741fc2a7d48da1cc
Danny Yassine <dannyyassine@gmail.com>
2024-03-23 07:31:15 -0400

    created test for application

---
+++ tests/application_integration_test.rs
@@ -0,0 +1,18 @@
+use rust_queue::models::{app_state::AppStateManager, application::Application};
+
+mod common;
+use common::set_up;
+
+#[tokio::test]
+async fn it_should_create_connection_into_app_state_manager() {
+    set_up();
+
+    Application::bootstrap().await;
+
+    assert!(AppStateManager::get_instance()
+        .get_state()
+        .as_ref()
+        .unwrap()
+        .connection
+        .is_some());
+}
