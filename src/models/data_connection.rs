df15cd6d6c98cfab98841e11fb1777cb0f8a4707
Danny Yassine <dannyyassine@gmail.com>
2024-03-26 18:42:53 -0400

    env vars

--- src/models/data_connection.rs
+++ src/models/data_connection.rs
@@ -6,12 +6,12 @@ pub struct DatabaseConnection {}
 
 impl DatabaseConnection {
     pub fn create() -> PgPool {
-        let result: Result<String, VarError> = env::var("DATABASE_URL");
-        let database_url = &result.unwrap();
+        let database_url = env::var("DATABASE_URL").unwrap();
+        let database_pool = env::var("DATABASE_POOL").unwrap().parse::<u32>().unwrap();
 
         let connection = PgPoolOptions::new()
-            .max_connections(16) // Set the maximum number of connections
-            .connect_lazy(database_url)
+            .max_connections(database_pool) // Set the maximum number of connections
+            .connect_lazy(&database_url)
             .expect("Failed to create pool");
 
         return connection;
